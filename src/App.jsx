dbc5f3ea67f2fc155699424fefe2389958c32ddc
ATaoyoyo <1029187998@qq.com>
2021-08-07 10:16:07 +0800

    init project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import routes from './router'
+import { ConfigProvider } from 'zarm'
+import zhCN from 'zarm/lib/config-provider/locale/zh_CN'
+import 'zarm/dist/zarm.css'
+import './App.css'
+
+function App() {
+  return <Router>
+    <ConfigProvider primaryColor={ '#007fff' } locale={ zhCN }>
+      <Switch>
+        {
+          routes.map(route => <Route exact key={ route.path } path={ route.path }>
+            <route.component />
+          </Route>)
+        }
+      </Switch>
+    </ConfigProvider>
+
+  </Router>
+}
+
+export default App
---
+++ src/container/Index/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'zarm'
+
+function Index() {
+  return <div>
+    <Button theme='primary'>Button</Button>
+  </div>
+}
+
+export default Index
