e8a14c95bab7b14561c4319437169dd3dcab6ac9
ATaoyoyo <1029187998@qq.com>
2021-08-16 22:35:58 +0800

    home静态页面

---
+++ src/components/BillItem/index.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import dayjs from 'dayjs'
+
+import { Cell } from 'zarm'
+import { useHistory } from 'react-router-dom'
+
+// import { typeMap } from '/@/utils'
+import CustomIcon from '../CustomIcon'
+
+import './style.less'
+
+const BillItem = ({ bill }) => {
+  const history = useHistory()
+  const [income, setIncome] = useState(0) // 收入
+  const [expense, setExpense] = useState(0) // 支出
+
+  useEffect(() => {
+    const _income = bill.bills
+      .filter(({ pay_type }) => pay_type === 2)
+      .reduce((cur, item) => {
+        cur += Number(item.amount)
+        return cur
+      }, 0)
+
+    const _expense = bill.bills
+      .filter(({ pay_type }) => pay_type === 1)
+      .reduce((cur, item) => {
+        cur += Number(item.amount)
+        return cur
+      }, 0)
+
+    setIncome(_income)
+    setExpense(_expense)
+  }, [bill.bills])
+
+  // 前往账单详情
+  const handToDetail = (item) => {
+    history.push(`/detail?id=${item.id}`)
+  }
+
+  return (
+    <div className="bill-item">
+      <div className="bill-item-header">
+        <span className="bill-item-header-date">2021-08-16</span>
+        <div className="bill-item-header-bills">
+          <p className="bill-item-header-bills-expense">
+            <img src="//s.yezgea02.com/1615953405599/zhi%402x.png" alt="支" />
+            <span className="value">¥ {expense.toFixed(2)}</span>
+          </p>
+          <p className="bill-item-header-bills-income">
+            <img src="//s.yezgea02.com/1615953405599/shou%402x.png" alt="收" />
+            <span className="value">¥ {income.toFixed(2)}</span>
+          </p>
+        </div>
+      </div>
+
+      <div className="bill-item-pane">
+        <div className="bill-item-pane-type">
+          <span className="bill-item-pane-type-name">
+            <CustomIcon type="canyin" />
+            <span>奖金</span>
+          </span>
+          <span className="bill-item-pane-type-money">
+            <CustomIcon />
+            <span>+5800.00</span>
+          </span>
+        </div>
+        <p className="bill-item-pane-time">22:00</p>
+      </div>
+    </div>
+  )
+}
+
+BillItem.propTypes = {
+  bill: PropTypes.object,
+}
+
+export default BillItem
--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Icon } from 'zarm'
+
+import BillItem from '/@/components/BillItem'
+
+import './style.less'
 
 const Home = () => {
-  return <div>Home</div>
+  const bills = [
+    {
+      bills: [
+        {
+          amount: '25.00',
+          date: '1623390740000',
+          id: 911,
+          pay_type: 1,
+          remark: '',
+          type_id: 1,
+          type_name: '餐饮',
+        },
+      ],
+      date: '2021-06-11',
+    },
+  ]
+  const [list, setList] = useState(bills)
+
+  return (
+    <div>
+      <div className="home">
+        <div className="home-header">
+          <div className="home-header-data-wrap">
+            <span className="home-header-data-wrap-expense">
+              总支出： <b>¥ 200.00</b>
+            </span>
+            <span className="home-header-data-wrap-income">
+              总收入： <b>¥ 200.00</b>
+            </span>
+          </div>
+
+          <div className="home-header-type-wrap">
+            <div className="home-header-type-wrap-type">
+              <span className="label">
+                <span>全部类型</span>
+                <Icon type="arrow-bottom" />
+              </span>
+            </div>
+            <div className="home-header-type-wrap-time">
+              <span className="time">
+                <span>2020-06</span>
+                <Icon type="arrow-bottom" />
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="home-content">
+          {list.map((item) => (
+            <BillItem bill={item} key={item.date} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
