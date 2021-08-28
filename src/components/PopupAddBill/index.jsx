86784ce33bae650e87ae0a2e4ecf90631b5172ba
ATaoyoyo <1029187998@qq.com>
2021-08-28 20:18:10 +0800

    虚拟键盘

--- src/components/PopupAddBill/index.jsx
+++ src/components/PopupAddBill/index.jsx
@@ -1,15 +1,19 @@
-import React, { forwardRef, useState } from 'react'
+import React, { forwardRef, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import { Icon, Popup } from 'zarm'
+import dayjs from 'dayjs'
+import { Icon, Input, Keyboard, Popup } from 'zarm'
 
 import CustomIcon from '/@/components/CustomIcon'
+import PopupDate from '/@/components/PopupDate'
 import './style.less'
 
 const PopupAddBill = forwardRef((props, ref) => {
+  const dateRef = useRef()
   const [payType, setPayType] = useState('expense')
   const [show, setShow] = useState(true)
-  const [money, setMoney] = useState()
+  const [date, setDate] = useState(new Date())
+  const [money, setMoney] = useState('')
   const [expenseType, setExpenseType] = useState('canyin')
   const [incomeType, setIncomeType] = useState('gongzi')
 
@@ -18,6 +22,38 @@ const PopupAddBill = forwardRef((props, ref) => {
       show: () => setShow(true),
       close: () => setShow(false),
     }
+  }
+
+  const handKeyClick = (key) => {
+    console.log(key)
+
+    if (key === 'delete') {
+      const val = money.slice(0, money.length - 1)
+      setMoney(val)
+      return
+    }
+
+    if (key === 'ok') {
+      return
+    }
+
+
+    const state =
+      key !== '.' &&
+      money.includes('.') &&
+      money &&
+      money.split('.')[1].length >= 2
+    // 当输入的值为 '.' 且 已经存在 '.'，则不让其继续字符串相加。
+    if (key === '.' && money.includes('.')) return
+    // 小数点后保留两位，当超过两位时，不让其字符串继续相加。
+    if (state) return
+    const val = money + key
+    setMoney(val)
+  }
+
+  const handSelectDate = (date) => {
+    console.log(date)
+    setDate(date)
   }
 
   return (
@@ -29,7 +65,10 @@ const PopupAddBill = forwardRef((props, ref) => {
       mountContainer={() => document.body}
     >
       <div className="popup-add-bill__wrapper">
-        <div className="popup-add-bill__wrapper__close">
+        <div
+          className="popup-add-bill__wrapper__close"
+          onClick={() => setShow(false)}
+        >
           <Icon type="wrong" />
         </div>
 
@@ -50,14 +89,20 @@ const PopupAddBill = forwardRef((props, ref) => {
               收入
             </span>
           </div>
-          <div className="popup-add-bill__wrapper__date">
-            <span>08-26</span> <Icon className="icon" type="arrow-bottom" />
+          <div
+            className="popup-add-bill__wrapper__date"
+            onClick={() => dateRef.current && dateRef.current.show()}
+          >
+            <span>{dayjs(date).format('MM-DD')}</span>{' '}
+            <Icon className="icon" type="arrow-bottom" />
           </div>
+
+          <PopupDate ref={dateRef} onSelect={handSelectDate} />
         </div>
 
         <div className="popup-add-bill__wrapper__money">
           <span className="icon">¥</span>
-          <span className="num">7182938</span>
+          <span className="num">{money}</span>
         </div>
 
         <div className="popup-add-bill__wrapper__bill-types">
@@ -70,6 +115,19 @@ const PopupAddBill = forwardRef((props, ref) => {
             </li>
           </ul>
         </div>
+      </div>
+
+      <div className="popup-add-bill__wrapper__remark">
+        <Input
+          className="remark-input"
+          type="text"
+          rows={4}
+          placeholder="请输入备注"
+        />
+      </div>
+
+      <div className="popup-add-bill__wrapper__keyboard">
+        <Keyboard type="price" onKeyClick={(key) => handKeyClick(key)} />
       </div>
     </Popup>
   )
